e35931316e11a33c0e80c0e5c79cb9f24992a417
Bhavesh <nar.bhavesh@gmail.com>
2016-06-24 16:41:51 +0530

    first commit

---
+++ pixelart.js
@@ -0,0 +1,6 @@
+
+var chhotabox= "boxo";
+	for(var i=0; i<=500; i++)
+{
+document.getElementById("bigbox").innerHTML += "<div id='chhotabox' onmouseover='this.style.backgroundColor=color;'></div>;
+}
